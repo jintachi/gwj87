611c66e7e17aaaab92dbd0f689c92d41ee3b143c
ModeRogueGames <moderoguegames@gmail.com>
2025-11-23 13:05:26 -0500

    Revert "Merge branch 'Alex's-Branch' into Dave's-New-Branch"
    This reverts commit cc7bd48cf52ee02d93f0366a86ed3d2017e6b4da, reversing
    changes made to 683370db132fefb2a796640baf9533ad784279f1.

--- mechanics/platforms/moving_platform.gd
+++ mechanics/platforms/moving_platform.gd
@@ -165,8 +165,6 @@ func _on_body_entered(body: Node2D) -> void:
 	if body == self:
 		return
 	
-	body.on_moving_platform = true
-	
 	# Add to tracking array if not already present
 	if body not in _on_platform_entities:
 		_on_platform_entities.append(body)
@@ -175,8 +173,6 @@ func _on_body_entered(body: Node2D) -> void:
 ## Called when an entity exits the detection area.
 func _on_body_exited(body: Node2D) -> void:
 	# Remove from tracking array
-	body.on_moving_platform = false
-
 	var index: int = _on_platform_entities.find(body)
 	if index >= 0:
 		_on_platform_entities.remove_at(index)
