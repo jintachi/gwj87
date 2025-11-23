78fa4ad544449b4de875cbd84e1843fa84d2b8ae
Alex <143654953+Cityships@users.noreply.github.com>
2025-11-22 23:04:35 -0600

    Adjustments to storage wall script. Fall hazard, player, rad chunk need adjustments to layers to properly let fall_hazard.gd trigger.

--- scenes/level1/fall_hazard.gd
+++ scenes/level1/fall_hazard.gd
@@ -1,4 +1,10 @@
 extends Area2D
 
 func _ready() -> void:
-    body_entered.connect(func(value):print("no"),)
+    body_entered.connect(
+        func(value):
+            if value is Player:
+                print("respawn player")
+            elif value is RadChunk:
+                print("send rad chunk to reasonable location"),
+    )
